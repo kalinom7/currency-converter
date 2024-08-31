5bd18547590e96a75a0d82098722189750286b21
Michał Kalinowski <michal@kalinowski.one>
2024-08-31 13:27:10 +0200

    add chf

--- src/exchange_rates.hpp
+++ src/exchange_rates.hpp
@@ -8,13 +8,15 @@ class ExchangeRates {
  private:
   std::map<std::string, std::map<std::string, double>> rates = {
       {"PLN", {{"USD", 0.25}, {"CHF", 0.23}}},
-      {"USD", {{"PLN", 3.8}, {"CHF", 0.86}}}};
+      {"USD", {{"PLN", 3.8}, {"CHF", 0.86}}},
+      {"CHF", {{"PLN", 4.57}, {"USD", 1.18}}},
+  };
 
  public:
   ExchangeRates() {}
-  
+
   double get_rate(std::string input_currency, std::string output_currency);
   bool check_for_input_currency(std::string input_currency);
-  bool check_for_output_currency(std::string input_currency, std::string output_currency);
-  
+  bool check_for_output_currency(std::string input_currency,
+                                 std::string output_currency);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,15 @@ int main(int argc, char *argv[]) {
 
   Validator validator = Validator(exchange_rates);
 
-  if (!validator.is_argc_valid(argc) || !validator.is_input_currency_valid(argv[2]) ||
-      !validator.is_output_currency_valid(argv[2],argv[3]) ) 
-  {
+  if (!validator.is_argc_valid(argc) ||
+      !validator.is_input_currency_valid(argv[2]) ||
+      !validator.is_output_currency_valid(argv[2], argv[3])) {
     return -1;
   }
 
   Converter converter =
       Converter(exchange_rates, std::stod(argv[1]), argv[2], argv[3]);
 
- 
   std::cout << converter.get_input_amount() << " in " << argv[2] << " is "
             << converter.convert() << " in " << argv[3] << std::endl;
 
